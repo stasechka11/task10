11c6c64d2dc4b4acd425d63abbb54d9b64c489b0
Anastasiia Fokina <stasechka11@yandex.ru>
2023-12-23 20:30:33 +0300

    Added lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,10 +1,17 @@
 package ru.netology;
 
+import lombok.Getter;
+
 public class Radio {
+    @Getter
     private int channelsQuantity;
+    @Getter
     private int minChannel;
+    @Getter
     private int maxChannel;
+    @Getter
     private int currentChannel;
+    @Getter
     private int currentVolume;
     private int minVolume = 0;
     private int maxVolume = 100;
@@ -19,22 +26,6 @@ public class Radio {
         this.channelsQuantity = channelsQuantity;
         this.minChannel = 0;
         this.maxChannel = channelsQuantity - 1;
-    }
-
-    public int getChannelsQuantity() {
-        return channelsQuantity;
-    }
-
-    public int getMinChannel() {
-        return minChannel;
-    }
-
-    public int getMaxChannel() {
-        return maxChannel;
-    }
-
-    public int getCurrentChannel() {
-        return currentChannel;
     }
 
     public void setCurrentChannel(int newCurrentChannel) {
@@ -57,10 +48,6 @@ public class Radio {
         } else {
             setCurrentChannel(getCurrentChannel() - 1);
         }
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
     }
 
     public void setMaxVolume() {
